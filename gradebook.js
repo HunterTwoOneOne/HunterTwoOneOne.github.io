363dd76dd2229ad800b67cc8f986c3c9f2cd7039
HunterTwoOneOne <nathanael_long@outlook.com>
2025-07-09 14:53:06 -0400

    Added Gradebook.html & .js
    Added Gradebook Files

---
+++ gradebook.js
@@ -0,0 +1,8 @@
+function fetchGradebookData() {
+    console.log("Fetching grade data...");
+}
+function populateGradebookTable(Data) {
+    console.log("Populating gradebook with data:", Data);
+}
+const gradedata = fetchGradebookData();
+populateGradebookTable(gradedata); 
